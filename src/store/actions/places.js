f02183be1e467a9915dd7047d29386001d348281
Mark Ammari <mark.ammari@gmail.com>
2019-04-30 14:40:07 -0400

    firebase deploy give npm err, (needs to be fixed)

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -6,15 +6,15 @@ export const addPlace = (placeName, location, image) => {
             name: placeName,
             location: location
         }
-        fetch("https://awesome-places-238720.firebaseio.com/places.json", {
-            method: 'POST',
-            body: JSON.stringify(placeData)
-        })
-        .catch(err => console.log(err))
-        .then(res => res.json())
-        .then(parsedRes => {
-            console.log(parsedRes)
-        })
+        // fetch("https://awesome-places-238720.firebaseio.com/places.json", {
+        //     method: 'POST',
+        //     body: JSON.stringify(placeData)
+        // })
+        // .catch(err => console.log(err))
+        // .then(res => res.json())
+        // .then(parsedRes => {
+        //     console.log(parsedRes)
+        // })
     }
 };
 
